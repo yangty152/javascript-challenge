354a96be7b3d2fa6b57a34d004b9dc236493b7df
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-06-15 21:29:18 -0400

    worked on level 1 js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,30 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// from data.js
+var tbody = d3.select("tbody");
+
+// YOUR CODE HERE!
+data.forEach(function(row){
+    console.log(row);
+    var tr = tbody.append("tr");
+
+    Object.entries(row).forEach(function([key, value]){
+        console.log(key,value);
+        var cell =tr.append("td");
+        cell.text(value);
+        });
+    });
+
+var button =d3.select("#filter-btn");
+function handleChange(event) {
+    // grab the value of the input field
+    var inputText = d3.event.target.value;
+    console.log(inputText)
+    data.forEach(function(row){
+        Object.entries(row).forEach(function([key,value]){
+        }
+    };
+  };
+  
+  button.on("click", handleChange);
