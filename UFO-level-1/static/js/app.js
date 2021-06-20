2d429344cc361a6e97b3ead86cee2bf6671bd105
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-06-19 21:10:43 -0400

    showed filtered data in the webpage

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,6 +21,7 @@ var button =d3.select("#filter-btn");
 // Select the form
 var form = d3.select("form");
 function handleChange() {
+    d3.select("#ufo-table").selectAll("tr").remove();
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // grab the value of the input field
@@ -29,6 +30,17 @@ function handleChange() {
     console.log(inputText)
     var filteredData = tableData.filter(tableData => tableData.datetime === inputText);
     console.log(filteredData)
+    filteredData.forEach(function(row){
+        
+        console.log(row);
+        var tr = tbody.append("tr");
+    
+        Object.entries(row).forEach(function([key, value]){
+            console.log(key,value);
+            var cell =tr.append("td");
+            cell.text(value);
+            });
+        });
 };
   
   button.on("click", handleChange);
