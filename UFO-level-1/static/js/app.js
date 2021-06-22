74d4155c13dbf13f7e002c07bdb848116769ee5c
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-06-21 22:48:02 -0400

    final clean up and added commnets

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,62 +2,54 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// from data.js
+// select html elements
 var tbody = d3.select("tbody");
+var button =d3.select("#filter-btn");
+var form = d3.select("form");
 
 // YOUR CODE HERE!
+// append table to display data from data.js
 data.forEach(function(row){
-    console.log(row);
     var tr = tbody.append("tr");
-
     Object.entries(row).forEach(function([key, value]){
-        console.log(key,value);
         var cell =tr.append("td");
         cell.text(value);
         });
     });
 
-var button =d3.select("#filter-btn");
-// Select the form
-var form = d3.select("form");
+// function to display filtered data when Filter Table button is clicked or Enter is pressed on the keyboard after input value in the Enter a Data field.
 function handleChange() {
+    //clear the table
     d3.select("#ufo-table").selectAll("tr").remove();
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // grab the value of the input field
     var datetimeElement = d3.select("#datetime");
     var datetimeValue = datetimeElement.property("value");
+    // when there is a date value, then filtered data based on the input value
     if (datetimeValue !== ""){
         var filteredData = tableData.filter(tableData => tableData.datetime === datetimeValue);
-        console.log(filteredData);
         //Display filtered data in the table
         filteredData.forEach(function(row){
-        
-        console.log(row);
-        var tr = tbody.append("tr");
-    
+        var tr = tbody.append("tr");  
         Object.entries(row).forEach(function([key, value]){
-            console.log(key,value);
             var cell =tr.append("td");
             cell.text(value);
             });
         });
     }
     
+    // when the value is removed, then display all data again.
     if(datetimeValue === ""){
         data.forEach(function(row){
-            console.log(row);
-            var tr = tbody.append("tr");
-        
+            var tr = tbody.append("tr");       
             Object.entries(row).forEach(function([key, value]){
-                console.log(key,value);
                 var cell =tr.append("td");
                 cell.text(value);
                 });
             });
     }
-    
 };
   
-  button.on("click", handleChange);
-  form.on("submit",handleChange);
+button.on("click", handleChange);
+form.on("submit",handleChange);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,32 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// from data.js
-var tableData = data;
-
-// YOUR CODE HERE!
-// from data.js
+// select html elements
 var tbody = d3.select("tbody");
+var button1 =d3.select("#filter-btn");
+var button2 =d3.select("#clear-filter-btn");
+var form = d3.select("form");
 
 // YOUR CODE HERE!
+// append table to display data from data.js
 data.forEach(function(row){
     var tr = tbody.append("tr");
-
     Object.entries(row).forEach(function([key, value]){
         var cell =tr.append("td");
         cell.text(value);
         });
     });
 
-var button1 =d3.select("#filter-btn");
-var button2 =d3.select("#clear-filter-btn");
-// Select the form
-var form = d3.select("form");
+// function to display filtered data when Filter Table button is clicked or Enter is pressed on the keyboard after input value in the Enter a Data field.
 function handleChange() {
+    //clear the table
     d3.select("#ufo-table").selectAll("tr").remove();
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    // grab the value of the input field
+    // grab the values of the input fields
     var datetimeElement = d3.select("#datetime");
     var datetimeValue = datetimeElement.property("value");
     var cityElement = d3.select("#city");
@@ -38,8 +35,10 @@ function handleChange() {
     var countryValue = countryElement.property("value");
     var shapeElement = d3.select("#shape");
     var shapeValue = shapeElement.property("value");
-    //var dict = []
+    
+    //create a filter dictionary
     var filters = new Object();
+    //assign key and value pair to the dictionary based on user inputs
     if (datetimeValue !== ""){
         filters["datetime"]=datetimeValue;
     }
@@ -56,26 +55,25 @@ function handleChange() {
         filters["shape"]=shapeValue;
     }
 
+    //call seaerch function to return filtered data 
     var filteredData = tableData.filter(search, filters);
     
+    //search function that will search data based on filters in a dictionary
     function search(data){
       return Object.keys(this).every((key) => data[key] === this[key]);
     }
 
+    //display filtered data in the html table
     filteredData.forEach(function(row){
-        
-        console.log(row);
         var tr = tbody.append("tr");
-    
         Object.entries(row).forEach(function([key, value]){
-            console.log(key,value);
             var cell =tr.append("td");
             cell.text(value);
             });
     });
-    console.log(filteredData);
 };
 
+//this function will empty all input values, and relist all values in the data.js
 function filterReset(){
     d3.select("#datetime").property("value","");
     d3.select("#city").property("value","");
@@ -84,8 +82,7 @@ function filterReset(){
     d3.select("#shape").property("value","");
     data.forEach(function(row){
         console.log(row);
-        var tr = tbody.append("tr");
-    
+        var tr = tbody.append("tr");   
         Object.entries(row).forEach(function([key, value]){
             console.log(key,value);
             var cell =tr.append("td");
@@ -94,6 +91,7 @@ function filterReset(){
         });
 }
 
+//events that triggers functions above
   button1.on("click", handleChange);
   button2.on("click", filterReset);
   form.on("submit",handleChange);
